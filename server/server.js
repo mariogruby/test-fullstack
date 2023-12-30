7b29c488673dbf913c1e003ff76872c5029e2c26
mario ruby <mariogabrielrn@gmail.com>
2023-12-30 02:55:43 +0100

    server format documet

--- server/server.js
+++ server/server.js
@@ -11,35 +11,40 @@ const PORT = process.env.PORT || 5005;
 const TOKEN = process.env.TOKEN;
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/products', async (req, res) => {
     try {
-        const response = axios.await.get(' https://test-fullstack.myshopify.com/admin/api/2023-04/products.json', {
-        headers: { 'X-Shopify-Access-Token': TOKEN,
-        'Content-Type': 'application/json'
-    }
-})
+        const response = await axios.get(' https://test-fullstack.myshopify.com/admin/api/2023-04/products.json', {
+            headers:
+            {
+                'X-Shopify-Access-Token': TOKEN,
+                'Content-Type': 'application/json'
+            }
+        })
 
-res.json(response.data.products);
-} catch (error) {
-  console.log(error);
-  res.status(500).json({ message: 'Internal server error' });
-}
+        res.json(response.data.products);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
 });
 
 app.get('products/:id', async (req, res) => {
+    const id = req.params.id;
     try {
-        const response = axios.await.get(`https://test-fullstack.myshopify.com/admin/api/2023-04/products/${id}.json`,{
-            headers:{ 'X-Shopify-Access-Token': TOKEN,
-        'Content-Type': 'application/json'
-    }
+        const response = await axios.get(`https://test-fullstack.myshopify.com/admin/api/2023-04/products/${id}.json`, {
+            headers:
+            {
+                'X-Shopify-Access-Token': TOKEN,
+                'Content-Type': 'application/json'
+            }
         })
         res.json(response.data.product);
-    }catch (error){
+    } catch (error) {
         console.log(error)
-        res.status(500).json({message:'Internal server error'});
+        res.status(500).json({ message: 'Internal server error' });
     }
 });
 
-app.listen(PORT, ()=> console.log(`Server listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
