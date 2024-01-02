23bf90291c546886fd24763ae647180a69860497
mario ruby <mariogabrielrn@gmail.com>
2024-01-02 15:10:55 +0100

    pequeñas modificaciones por actualizacion del server

--- client/src/services/api.service.js
+++ client/src/services/api.service.js
@@ -5,7 +5,7 @@ const ApiService = {
     //llamada a todos los productos
     fetchProducts: async () => {
         try {
-            const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/products`);
+            const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api/products`);
             return response.data;
         } catch (error) {
             throw new Error('Error fetching products:', error);
@@ -15,7 +15,7 @@ const ApiService = {
     // llamada a el producto por  el id 
     fetchProductById: async (id) => {
         try {
-            const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/products/${id}`);
+            const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api/products/${id}`);
             return response.data;
         } catch (error) {
             throw new Error('Error fetching product:', error);
